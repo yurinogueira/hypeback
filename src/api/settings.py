6fd551f6b11f6d97dedfdf24bafdf94269631cfa
yurinogueira <yurinogueira@id.uff.br>
2022-02-07 00:49:04 -0300

    chore: improve security

--- src/api/settings.py
+++ src/api/settings.py
@@ -26,8 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)#wu1_$63u!tp!whi@5wr-%cd_6enoy3$2^@=0)^&b6+-j%4=h"
+SECRET_KEY = env.str("SECRET_KEY", default="")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -58,7 +57,6 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "HypeBack.urls"
 
 TEMPLATES = [
     {
@@ -76,12 +74,15 @@ TEMPLATES = [
     },
 ]
 
+
+# APLICATION SETTINGS
+# ---------------------------------------------------------------------------------------------------------------------
+ROOT_URLCONF = "api.urls"
 WSGI_APPLICATION = "api.wsgi.application"
 
 
-# FILES
+# DIRECTORY SETTINGS
 # ---------------------------------------------------------------------------------------------------------------------
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = urllib.parse.urljoin(env.str("STATIC_HOST", default=""), "/static/")
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
