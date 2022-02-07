6cd4092d74a84bf6034c4f2f0d806736daa0bdd1
yurinogueira <yurinogueira@id.uff.br>
2022-02-07 01:55:07 -0300

    feature: setup code quality

--- src/api/urls.py
+++ src/api/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.views.static import serve
 from django.urls import path, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
