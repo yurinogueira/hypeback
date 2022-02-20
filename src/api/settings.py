26ff0ab6b21e932fb73105b2fb3a411a471dd76a
yurinogueira <yurinogueira@id.uff.br>
2022-02-19 23:20:47 -0300

    feature: adjust csrf

--- src/api/settings.py
+++ src/api/settings.py
@@ -31,6 +31,7 @@ SECRET_KEY = env.str("SECRET_KEY", default="")
 DEBUG = True
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["*"])
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS", default=["*"])
 
 
 # Application definition
